d2989987eb95c89b41dbeaa1b091b67ba2804f9f
Elena Zhuvak <elenkazh@gmail.com>
2024-09-08 21:18:39 +0100

    adds css files

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,12 @@
-console.log("Gallery");
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+console.log(SimpleLightbox);
