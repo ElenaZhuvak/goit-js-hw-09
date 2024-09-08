dc4e76e5a8b7e1a1a5b856731891d97588b35d67
ElenaZhuvak <elenkazh@gmail.com>
2024-09-08 20:19:11 +0000

    Deploying to gh-pages from @ ElenaZhuvak/goit-js-hw-09@d2989987eb95c89b41dbeaa1b091b67ba2804f9f 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,2 +1,2 @@
-import"./assets/styles-B4s-4mBC.js";console.log("Gallery");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as o}from"./assets/vendor-CgTBfC_f.js";new o(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250});console.log(o);
 //# sourceMappingURL=1-gallery.js.map
