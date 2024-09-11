2e40128811f6235ae2b92b8f094d1dd00ab52c7f
Elena Zhuvak <elenkazh@gmail.com>
2024-09-11 22:24:41 +0100

    adds form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,42 @@
-console.log('Form');
+let formData = {
+    email: "",
+    message: "",
+}
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input');
+const messageArea = form.querySelector('textarea');
+
+const STORAGE_KEY = "feedback-form-state";
+
+form.addEventListener('input', handelData);
+function handelData () {
+    formData.email = emailInput.value.trim();
+    formData.message = messageArea.value.trim();
+
+    formData = {
+        email: formData.email,
+        message: formData.message
+    }
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  
+}
+
+form.addEventListener('submit', handleFormSubmit);
+function handleFormSubmit(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+  
+    if (formData.email === '' || formData.message === '') {
+        alert ('Fill please all fields');
+        return
+    }
+    console.log(formData);
+    
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+    formData = {
+        email: "",
+        message: ""
+    }
+}
